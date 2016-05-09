b2e6ede20b77a3685ed3e745d83b31e832fa5754
Endika Iglesias García <eiglesias@qdqmedia.com>
2016-05-09 10:51:33 +0200

    [ADD] add moon phase

--- cleanMyCar.go
+++ cleanMyCar.go
@@ -21,11 +21,17 @@ const FROM string = "FROM EMAIL"
 const TO string = "TO EMAIL"
 const SUBJECT string = "Car Clean!! The Summary"
 
-func checkWeekDay(cDate time.Time) (result bool, days int) {
+func checkWeekDay(cDate time.Time) (result bool, days int, moon string) {
 	result = true
 	days = 0
+	moon = ""
 	for i := 0; i < 7; i++ {
-		if !checkHoursDay(cDate) {
+		moon_tmp := ""
+		result, moon_tmp = checkHoursDay(cDate)
+		if moon == "" {
+			moon = moon_tmp
+		}
+		if !result {
 			result = false
 			break
 		} else {
@@ -36,11 +42,16 @@ func checkWeekDay(cDate time.Time) (result bool, days int) {
 	return
 }
 
-func checkHoursDay(cDate time.Time) (result bool) {
+func checkHoursDay(cDate time.Time) (result bool, moon string) {
 	result = true
+	moon = ""
 	hoursList := [...]string{
 		"T07:00:00", "T10:00:00", "T12:00:00", "T13:00:00",
 		"T15:00:00", "T16:00:00", "T18:00:00", "T19:00:00"}
+	moonList := map[float64]string{
+		0: "New Moon", 0.25: "First quarter Moon", 0.5: "Full Moon",
+		0.75: "Last quarter Moon"}
+	moonRangeList := [...]float64{0, 0.25, 0.5, 0.75}
 	dangerList := [...]string{"rain", "snow", "sleet", "hail", "thunderstorm"}
 	for _, value := range hoursList {
 		checkDate := cDate.Format("2006-01-02") + value
@@ -54,6 +65,20 @@ func checkHoursDay(cDate time.Time) (result bool) {
 				break
 			}
 		}
+		if moon == "" {
+			for _, mRL := range moonRangeList {
+				MoonPhase := f.Daily.Data[0].MoonPhase
+				if MoonPhase <= mRL {
+					if val, ok := moonList[mRL]; ok {
+						moon = strconv.FormatFloat(MoonPhase, 'f', 6, 64)
+						moon = moon + " to " + val + " "
+						moon = moon + strconv.FormatFloat(mRL, 'f', 6, 64)
+						break
+					}
+				}
+			}
+		}
+
 		if !result {
 			break
 		}
@@ -81,14 +106,17 @@ func main() {
 	summary := ""
 	for i := 0; i < 7; i++ {
 		result := "NO"
-		r, d := checkWeekDay(cDate)
+		r, d, m := checkWeekDay(cDate)
 		if r {
 			result = "OK"
 		}
-		fmt.Printf("%s => %s (%d)\n", cDate.Format("2006-01-02"), result, d)
+		fmt.Printf(
+			"%s => %s Sunny days: %d Moon: %s\n",
+			cDate.Format("2006-01-02"), result, d, m)
 		summary = summary + " "
 		summary = summary + cDate.Format("2006-01-02") + " => "
-		summary = summary + result + " (" + strconv.Itoa(d) + ")<br>"
+		summary = summary + result + " Sunny days: " + strconv.Itoa(d)
+		summary = summary + " Moon: " + m + "<br>"
 		cDate = cDate.Add(time.Hour * 24)
 	}
 	sendMail(summary)
